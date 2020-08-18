1cf4f16777e3049df638ad9ca30da50492983081
levi <levi840714@gmail.com>
2020-08-19 01:14:19 +0800

    [fixed]modify column length

--- schema/job.go
+++ schema/job.go
@@ -8,14 +8,14 @@ var AllSchema = []interface{}{
 
 type Job struct {
 	ID       int       `gorm:"column:id;AUTO_INCREMENT"`
-	Company  string    `gorm:"column:Company;type:varchar(20);NOT NULL"`
+	Company  string    `gorm:"column:Company;type:varchar(80);NOT NULL"`
 	JobId    string    `gorm:"column:JobId;type:varchar(40);UNIQUE;NOT NULL"`
-	Keyword  string    `gorm:"column:Keyword;type:varchar(20);NOT NULL"`
-	Title    string    `gorm:"column:Title;type:varchar(40);NOT NULL"`
+	Keyword  string    `gorm:"column:Keyword;type:varchar(40);NOT NULL"`
+	Title    string    `gorm:"column:Title;type:varchar(128);NOT NULL"`
 	Salary   string    `gorm:"column:Salary;type:varchar(40)"`
 	Content  string    `gorm:"column:Content;type:text"`
 	Link     string    `gorm:"column:Link;type:varchar(256);NOT NULL"`
-	Website  string    `gorm:"column:Website;type:varchar(12);NOT NULL"`
+	Website  string    `gorm:"column:Website;type:varchar(20);NOT NULL"`
 	Status   string    `gorm:"column:status;type:enum('0', '1');default:'1'"`
 	CreateAt time.Time `gorm:"column:createAt;type:datetime;NOT NULL"`
 }
