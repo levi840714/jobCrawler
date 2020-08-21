9a30cabcfc3b99fcf10a829bd7565bff658077d5
levi <golevi666@gmail.com>
2020-08-21 10:05:30 +0800

    [fix] modify JobId column length

--- schema/job.go
+++ schema/job.go
@@ -9,7 +9,7 @@ var AllSchema = []interface{}{
 type Job struct {
 	ID       int       `gorm:"column:id;AUTO_INCREMENT"`
 	Company  string    `gorm:"column:Company;type:varchar(80);NOT NULL"`
-	JobId    string    `gorm:"column:JobId;type:varchar(40);UNIQUE;NOT NULL"`
+	JobId    string    `gorm:"column:JobId;type:varchar(80);UNIQUE;NOT NULL"`
 	Keyword  string    `gorm:"column:Keyword;type:varchar(40);NOT NULL"`
 	Title    string    `gorm:"column:Title;type:varchar(128);NOT NULL"`
 	Salary   string    `gorm:"column:Salary;type:varchar(40)"`
